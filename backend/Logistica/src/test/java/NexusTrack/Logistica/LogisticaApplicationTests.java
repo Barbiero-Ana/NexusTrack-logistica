8959976703ee6382d30fb3e3ff2238c4746d8c80
Ana Barbiero <anacarolinabarbiero@gmail.com>
2025-04-26 19:41:01 -0400

    commit

---
+++ backend/Logistica/src/test/java/NexusTrack/Logistica/LogisticaApplicationTests.java
@@ -0,0 +1,13 @@
+package NexusTrack.Logistica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
